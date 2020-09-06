b9c8350286162eb8a7ae127ea960743ea81d928f
mosesjebish <mosesjebish@users.noreply.github.com>
2020-09-07 01:28:56 +0530

    Bookmark Manager Service

---
+++ src/test/java/com/mosesjebish/bookmarkmanager/BookmarkManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mosesjebish.bookmarkmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
