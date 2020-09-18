080449e14793fff22834c747b5e4a6e32a2568e9
mosesjebish <mosesjebish@users.noreply.github.com>
2020-09-18 10:20:04 +0530

    With changes for Oauth2 security [fixed]

--- src/main/java/com/mosesjebish/bookmarkmanager/Application.java
+++ src/main/java/com/mosesjebish/bookmarkmanager/Application.java
@@ -2,8 +2,12 @@ package com.mosesjebish.bookmarkmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableResourceServer
--- src/main/java/com/mosesjebish/bookmarkmanager/security/WebSecurityConfiguration.java
+++ src/main/java/com/mosesjebish/bookmarkmanager/security/WebSecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.mosesjebish.bookmarkmanager.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,14 +9,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/v2/api-docs",
-                "/configuration/ui",
-                "/swagger-resources/**",
-                "/configuration/security",
-                "/swagger-ui.html**",
-                "/webjars/**");
-
+        web.ignoring().antMatchers("/swagger-ui/**", "/v3/api-docs/**");
     }
-
 
 }
